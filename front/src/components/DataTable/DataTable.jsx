acea5a585949bf96de3e463ae8c96b205c228d7b
Josue Roque <jroque@applaudstudios.com>
2021-12-27 15:58:13 -0600

    Added DataTable and Form components

--- front/src/components/DataTable/DataTable.jsx
+++ front/src/components/DataTable/DataTable.jsx
@@ -9,18 +9,8 @@ import Paper from "@mui/material/Paper";
 
 import "./DataTable.css";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-const DataTable = () => {
+const DataTable = ({ rows = [] }) => {
+  console.log(rows);
   return (
     <TableContainer
       component={Paper}
@@ -30,28 +20,26 @@ const DataTable = () => {
       <Table aria-label='simple table'>
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align='right'>Calories</TableCell>
-            <TableCell align='right'>Fat&nbsp;(g)</TableCell>
-            <TableCell align='right'>Carbs&nbsp;(g)</TableCell>
-            <TableCell align='right'>Protein&nbsp;(g)</TableCell>
+            <TableCell align='right'>First Name</TableCell>
+            <TableCell align='right'>LastName</TableCell>
+            <TableCell align='right'>Address</TableCell>
+            <TableCell align='right'>SSN</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component='th' scope='row'>
-                {row.name}
-              </TableCell>
-              <TableCell align='right'>{row.calories}</TableCell>
-              <TableCell align='right'>{row.fat}</TableCell>
-              <TableCell align='right'>{row.carbs}</TableCell>
-              <TableCell align='right'>{row.protein}</TableCell>
-            </TableRow>
-          ))}
+          {rows.length > 0
+            ? rows.map((row) => (
+                <TableRow
+                  key={row.SSN}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align='right'>{row.firstName}</TableCell>
+                  <TableCell align='right'>{row.lastName}</TableCell>
+                  <TableCell align='right'>{row.address}</TableCell>
+                  <TableCell align='right'>{row.SSN}</TableCell>
+                </TableRow>
+              ))
+            : ""}
         </TableBody>
       </Table>
     </TableContainer>
--- front/src/components/Form/Form.jsx
+++ front/src/components/Form/Form.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import DataTable from "../DataTable/DataTable";
 import Container from "@mui/material/Container";
-import { Button, FormGroup, Input } from "@mui/material";
+import { Button, FormGroup, Input, InputMask, TextField } from "@mui/material";
 import swal from "sweetalert";
 import { useState } from "react";
 
 const Form = () => {
   const [firstName, setFirstName] = useState("");
-  const [LastName, setLastName] = useState(null);
+  const [lastName, setLastName] = useState(null);
   const [address, setAddress] = useState(null);
   const [SSN, setSSN] = useState(null);
 
   const submit = () => {
-    swal({
+    /*     swal({
       title: "Good job!",
       text: "You clicked the button!",
       icon: "success",
-    });
+    }); */
   };
 
   console.log(firstName);
@@ -27,18 +27,45 @@ const Form = () => {
         justifyContent: "space-around",
       }}
     >
-      <FormGroup>
-        <Input
+      <FormGroup style={{ minWidth: 500, marginTop: 50 }}>
+        <TextField
           value={firstName}
           onChange={(e) => {
             setFirstName(e.target.value);
           }}
           required
           placeholder='First Name'
-        ></Input>
-        <Input required placeholder='Second Name'></Input>
-        <Input required placeholder='Address'></Input>
-        <Input required placeholder='SSN'></Input>
+          style={{ margin: 10 }}
+        ></TextField>
+        <TextField
+          value={lastName}
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+          required
+          placeholder='Second Name'
+          style={{ margin: 10 }}
+        ></TextField>
+        <TextField
+          value={address}
+          onChange={(e) => {
+            setAddress(e.target.value);
+          }}
+          required
+          placeholder='Address'
+          style={{ margin: 10 }}
+        ></TextField>
+        <TextField
+          mask='999-99-9999'
+          value={SSN}
+          onChange={(e) => {
+            setSSN(e.target.value);
+          }}
+          required
+          placeholder='SSN'
+          style={{ margin: 10 }}
+        ></TextField>
+
         <Button onClick={submit}> Submit </Button>
       </FormGroup>
       <DataTable></DataTable>
